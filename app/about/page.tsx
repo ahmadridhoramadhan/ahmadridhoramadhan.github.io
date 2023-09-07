290f2272a75cbdb5d1424ad28b212f0ae4744fc7
Ramadhan5 <90996720+Ramadhan5@users.noreply.github.com>
2023-09-07 23:28:38 +0800

    modified:   app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <main className="flex-auto">
-
+        <main className="flex-auto pt-16 px-3 md:pl-28 md:pt-20">
+            <h1 className="text-3xl mb-3">About</h1>
+            <p className="break-words pl-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur aperiam ab, unde deserunt expedita voluptatibus soluta qui, commodi ullam sapiente, velit veniam sit quae cumque quis libero vitae impedit a consectetur vel consequatur itaque culpa. Maxime aliquam cumque totam mollitia temporibus qui, nisi laborum natus blanditiis accusamus ullam quas fuga quam minima esse eos commodi quae iure, suscipit distinctio exercitationem. Quibusdam quidem repellendus rem non voluptatibus! Ipsa dicta ullam qui rem assumenda. Nisi rerum voluptates delectus ab fuga neque dolore accusantium dignissimos exercitationem velit corrupti saepe veritatis quae laudantium tenetur iure voluptas similique nostrum obcaecati, blanditiis voluptatibus doloribus distinctio. Velit!</p>
         </main>
     )
 }
